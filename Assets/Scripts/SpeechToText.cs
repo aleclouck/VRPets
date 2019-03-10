452d7d78df809d886d8bd3d4dced4f494e6642db
finkage <finkage@gmail.com>
2019-03-10 04:29:19 -0700

    Revert "Revert "finally fixed everything. honestly i have no idea how it worked the first time but now it works for sure.""
    This reverts commit 13b9e36a3950a8c7f1b277d99f043fe92873cf37.

--- Assets/Scripts/SpeechToText.cs
+++ Assets/Scripts/SpeechToText.cs
@@ -5,28 +5,25 @@ using UnityEngine.Windows.Speech;
 namespace VoiceDetection {
 
     public class SpeechToText : MonoBehaviour {
-        static private KeywordRecognizer treeLayer1;
-        static private List<string> layer1Keys;
-        static private Dictionary<string, bool> keywordActive;
+        private KeywordRecognizer treeLayer1;
+        private List<string> layer1Keys = new List<string>();
+        private Dictionary<string, bool> keywordActive = new Dictionary<string, bool>();
 
         // Start is called before the first frame update
         void Start() {
-            layer1Keys = new List<string>();
-
-            keywordActive = new Dictionary<string, bool>();
-
             treeLayer1 = new KeywordRecognizer(layer1Keys.ToArray());
             treeLayer1.OnPhraseRecognized += VoiceRecognized;
             treeLayer1.Start();
         }
 
-        // Update is called once per frame
+        // LateUpdate is called once per frame after all updates
         void LateUpdate() {
             foreach(string key in layer1Keys) {
                 keywordActive[key] = false;
             }
         }
 
+        //called when a keyword is recognized by voice detection
         private void VoiceRecognized(PhraseRecognizedEventArgs voice) {
             Debug.Log(voice.text);
             keywordActive[voice.text] = true;
@@ -36,9 +33,6 @@ namespace VoiceDetection {
         public void AddWord(string word) {
             layer1Keys.Add(word);
             keywordActive.Add(word, false);
-            treeLayer1 = new KeywordRecognizer(layer1Keys.ToArray());
-            treeLayer1.OnPhraseRecognized += VoiceRecognized;
-            treeLayer1.Start();
         }
 
         //main function used for checking if a word was heard
